392a897d052c0a6cc5009219b59be3e50d5f0d3c
gunhee9145 <gunheelee9145@gmail.com>
2024-08-30 07:27:33 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -16,6 +16,7 @@
 
 //모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //라우팅
@@ -25,6 +26,8 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.use("/", home); //미들 웨어를 등록해주는 메서드
 
